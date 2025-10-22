c8ed6a1f48615141f49fb2506463472c70a83333
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-10-22 07:30:32 +0300

    feat(car): implement Car module with CRUD operations and DTOs feat(auth-utils): make AuthUtilsModule global fix(car-type): change relationship from OneToOne to OneToMany with Car

--- src/car-type/car-type.module.ts
+++ src/car-type/car-type.module.ts
@@ -3,11 +3,11 @@ import { CarTypeService } from './car-type.service';
 import { CarTypeController } from './car-type.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarType } from 'src/typeorm/entities/car/car-type.entity';
-import { AuthUtilsModule } from 'src/auth-utils/auth-utils.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CarType]), AuthUtilsModule],
+  imports: [TypeOrmModule.forFeature([CarType])],
   controllers: [CarTypeController],
   providers: [CarTypeService],
+  exports: [CarTypeService],
 })
 export class CarTypeModule {}
